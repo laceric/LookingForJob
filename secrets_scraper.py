d7f8cfa74779677936430240ee225a0b155dfc1f
laceric <123101925+laceric@users.noreply.github.com>
2024-09-19 13:45:24 +0200

    Create secrets_scraper.py

---
+++ secrets_scraper.py
@@ -0,0 +1,8 @@
+# Constante pour API NOTION
+NOTION_API_KEY = "cle_api_notion"
+DATABASE_OFFRES_ID = "id_database_notion_offres"
+DATABASE_PARAM_SCRAP_OFFRES_ID = "id_database_notion_parametrage"
+
+# Constante pour session Selenium
+CHROME_PROFILE_PATH = 'chemin_du_profil_chrome_utilisé'
+EXECUTABLE_PATH_CHROME_SERVICE = 'chemin_du_profil_\\chromedriver.exe'
